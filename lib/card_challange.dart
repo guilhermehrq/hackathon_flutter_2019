529c9c4b1d0611ba6f80777e294f5afc6d35e927
João Vitor Rodrigues <jv-rodrigues1999@hotmail.com>
2019-06-01 14:54:54 -0300

    Fix cards

--- lib/card_challange.dart
+++ lib/card_challange.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CardChallenge extends StatefulWidget {
+  int _index;
+  String _title;
+  String _description;
 
+  CardChallenge(int index, String title, String description) {
+    this._index = index;
+    this._title = title;
+    this._description = description;
+  }
 
   @override
-  _CardChallengeState createState() => _CardChallengeState();
+  _CardChallengeState createState() => _CardChallengeState(_index, _title, _description);
 }
 
 class _CardChallengeState extends State<CardChallenge> {
+  int _index;
+  String _title;
+  String _description;
+
+  _CardChallengeState(int index, String title, String description) {
+    this._index = index;
+    this._title = title;
+    this._description = description;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,22 +39,23 @@ class _CardChallengeState extends State<CardChallenge> {
                   width: MediaQuery.of(context).size.width,
                   padding: EdgeInsets.all(16.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Row(
                         children: <Widget>[
                           Container(
                             margin: EdgeInsets.only(right: 4.0),
-                            child: Text('#1', style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold, color: Colors.green),),
+                            child: Text('#$_index', style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold, color: Colors.green),),
                           ),
                           Container(
                             margin: EdgeInsets.only(right: 4.0),
-                            child: Text('How are you?', style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),),
+                            child: Text(_title, style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),),
                           ),
                         ],
                       ),
                       Container(
                         margin: EdgeInsets.only(right: 4.0),
-                        child: Text('dsd sds s s ds dsds  sds ss sdsd sdsdsdsdsds?', style: TextStyle(fontSize: 16.0,),),
+                        child: Text(_description, style: TextStyle(fontSize: 16.0,),),
                       )
                     ],
                   ),
