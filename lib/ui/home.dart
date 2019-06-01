3e177a2c6b30b736f8037b2f6fbb1916430c8204
João Vitor Rodrigues <jv-rodrigues1999@hotmail.com>
2019-06-01 16:15:04 -0300

    finalizando

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -47,7 +47,7 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                       Text(
-                        'Desafios concluídos: 10',
+                        'Challenges completed: 10',
                         style: TextStyle(
                             color: Colors.white,
                             fontSize: 16
